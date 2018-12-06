5978118ce4c67c888dfda6c9f8dc7654e3ebb9ae
Kuzirashi <kuzirashi@interia.pl>
2018-12-06 15:01:38 +0100

    Add missing addresses.

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,6 @@ import Analytics from './analytics/Analytics';
 import EAC from './eac';
 import Util from './utils/Util';
 import GasPriceUtil from './utils/GasPriceUtil';
+import Constants from './Constants';
 
-export { Analytics, EAC, GasPriceUtil, Util };
+export { Analytics, Constants, EAC, GasPriceUtil, Util };
