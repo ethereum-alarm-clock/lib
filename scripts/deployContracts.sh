3725af057e94feb4089d70bf22ffaf4ac5615303
Daniel Kmak <kuzirashi@interia.pl>
2018-12-13 10:51:54 +0100

    Improve deploy scripts.

--- scripts/deployContracts.sh
+++ scripts/deployContracts.sh
@@ -1,13 +1,28 @@
 #!/bin/bash
+
+CWD="$( cd "$(dirname "$0")" ; pwd -P )"
+LIB_DIR=$CWD/../
+CONTRACTS_DIR=$CWD/../node_modules/@ethereum-alarm-clock/contracts
+TEST_ASSETS_FILE=$LIB_DIR/config/contracts/1002.json
+
+if [ ! -d "$CONTRACTS_DIR" ]; then
+    CONTRACTS_DIR=$CWD/../../contracts
+fi
+
 echo "Deploying contracts..."
-cd node_modules/@ethereum-alarm-clock/contracts
-truffle network --clean
-truffle migrate --reset
-cd ../../../
+cd "$CONTRACTS_DIR"
+npm run network-clean
+npm run migrate-reset
+cd "$LIB_DIR"
 
 echo "Moving the generated contract files..."
-rm config/contracts/test.json
 
-node scripts/extractContractsInfo.js development
+if [ -f "$TEST_ASSETS_FILE" ]; then
+    rm "$TEST_ASSETS_FILE"
+fi
+
+echo "$LIB_DIR/scripts/extractContractsInfo.js"
+
+node "$LIB_DIR/scripts/extractContractsInfo.js" development "$CONTRACTS_DIR/build/contracts"
 
 echo "Done."
